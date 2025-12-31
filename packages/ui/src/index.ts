03b7f17d7cbe70f498efe13d260272f91cb6fee0
Donovan Allen <dallen.928@gmail.com>
2025-12-30 19:20:23 -0500

    new UI components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,9 @@
 // Utility function
 export { cn } from './utils';
 
+// Hooks
+export { useIsMobile } from './hooks/use-mobile';
+
 // Components
 export { Badge, badgeVariants, type BadgeProps } from './components/badge';
 export { Button, buttonVariants, type ButtonProps } from './components/button';
@@ -12,6 +15,11 @@ export {
   CardDescription,
   CardContent,
 } from './components/card';
+export {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from './components/collapsible';
 export {
   Dialog,
   DialogPortal,
@@ -36,6 +44,26 @@ export {
   NavigationMenuIndicator,
   NavigationMenuViewport,
 } from './components/navigation-menu';
+export { Separator } from './components/separator';
+export {
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from './components/sheet';
+export { Skeleton } from './components/skeleton';
 export { Toaster } from './components/sonner';
 export { Switch } from './components/switch';
 export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/tabs';
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from './components/tooltip';
